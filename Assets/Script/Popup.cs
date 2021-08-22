a647e0e123704e4d40b23d8fb1341de3988dcc87
Giovanna Cazelato Pires <ginha_capires@hotmail.com>
2021-08-22 19:54:50 -0300

    v0.2

--- Assets/Script/Popup.cs
+++ Assets/Script/Popup.cs
@@ -7,16 +7,18 @@ public class Popup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("testinho");
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+
     }
 
-    public void Close()
+    void Close()
     {
         Debug.Log("TEEEEEEEEETE");
         this.gameObject.SetActive(false);
